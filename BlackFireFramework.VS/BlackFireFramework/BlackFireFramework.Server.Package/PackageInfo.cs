fbc82716bc3b42e15bd4a3d74b3f3be133088e03
Company\Alan <mr.alan.china@gmail.com>
2018-05-04 19:24:14 +0800

    添加服务器package api配置项目。

---
+++ BlackFireFramework.VS/BlackFireFramework/BlackFireFramework.Server.Package/PackageInfo.cs
@@ -0,0 +1,21 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2018 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+namespace BlackFireFramework
+{
+    [System.Serializable]
+    public sealed class PackageInfo
+    {
+        public string name;
+
+        public string version;
+
+        public string more;
+
+        public string url;
+
+    }
+}
---
+++ BlackFireFramework.VS/BlackFireFramework/BlackFireFramework.Server.Package/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using SimpleJson;
+
+namespace BlackFireFramework.Server.Package
+{
+    class Program
+    {
+        private const string ServerDomain = "http://localhost/packages/";
+
+        private const string ServerAPI = ServerDomain+"api.json";
+
+        private const string PackageReadmeFileName = "Readme.json";
+
+        static void Main(string[] args)
+        {
+            string currentDir = System.Environment.CurrentDirectory;
+
+            DirectoryInfo dirInfo = new DirectoryInfo(currentDir);
+
+            foreach (var nextFolder in dirInfo.GetDirectories())
+            {
+                Console.WriteLine(nextFolder.Name);
+
+                var json = File.ReadAllText(nextFolder.FullName+"/"+ PackageReadmeFileName);
+
+                PackageInfo packageInfo = SimpleJson.SimpleJson.DeserializeObject(json) as PackageInfo;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
