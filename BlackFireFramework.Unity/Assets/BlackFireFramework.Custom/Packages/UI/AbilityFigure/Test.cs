228ba90aab8e786a77dda03095828acc5ae2a1b3
DESKTOP-0PA8STI\root <mr.alan.china@gmail.com>
2018-06-12 18:40:58 +0800

    测试添加了package 中lyz的包。

---
+++ BlackFireFramework.Unity/Assets/BlackFireFramework.Custom/Packages/UI/AbilityFigure/Test.cs
@@ -0,0 +1,28 @@
+﻿using Lyz;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lyz
+{
+    public class Test : MonoBehaviour
+    {
+        AbilityFigure abilityFigure;
+        private void Start()
+        {
+            abilityFigure = this.GetComponent<AbilityFigure>();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                abilityFigure.AddDotFunc();
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+
+            }
+        }
+
+    }
+}
