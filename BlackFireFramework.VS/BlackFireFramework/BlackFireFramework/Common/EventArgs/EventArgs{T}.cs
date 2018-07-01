5acdf31d56b73d504914cc22fc8e5389174d650d
DESKTOP-P80QGVB\Mr-Alan <mr.alan.china@gmail.com>
2018-07-02 03:37:29 +0800

    添加SuperSocket客户端demo。

---
+++ BlackFireFramework.VS/BlackFireFramework/BlackFireFramework/Common/EventArgs/EventArgs{T}.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackFireFramework
+{
+    /// <summary>
+    /// 只带传递一个参数的事件参数。
+    /// </summary>
+    /// <typeparam name="T">目标参数类型。</typeparam>
+    public class EventArgs<T> : EventArgs
+    {
+        public EventArgs(T arg)
+        {
+            Arg = arg;
+        }
+
+        /// <summary>
+        /// 参数。
+        /// </summary>
+        public T Arg { get; }
+
+    }
+}
