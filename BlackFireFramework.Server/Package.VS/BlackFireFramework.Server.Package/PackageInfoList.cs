eb3161795f9814d999779ddc7561c72b19acf0ff
DESKTOP-P80QGVB\Mr-Alan <mr.alan.china@gmail.com>
2018-07-03 22:19:48 +0800

    调整项目目录结构。

---
+++ BlackFireFramework.Server/Package.VS/BlackFireFramework.Server.Package/PackageInfoList.cs
@@ -0,0 +1,28 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2018 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace BlackFireFramework
+{
+
+
+    [System.Serializable]
+    public sealed class PackageInfoList
+    {
+        public List<PackageInfoDic> packages;
+    }
+
+    [System.Serializable]
+    public sealed class PackageInfoDic
+    {
+        public string classify;
+
+        public List<PackageInfo> packageInfos;
+    }
+
+
+}
--- BlackFireFramework.VS/BlackFireFramework/BlackFireFramework.Test/FrameworkTest.cs
+++ BlackFireFramework.VS/BlackFireFramework/BlackFireFramework.Test/FrameworkTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics;
-using BlackFireFramework.TestAssembly;
-using BlackFireFramework.Unity;
 
 namespace BlackFireFramework.Test
 {
