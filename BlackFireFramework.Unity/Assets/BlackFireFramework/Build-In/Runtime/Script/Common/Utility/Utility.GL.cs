cb7389e633fa5c25a6936461eead64d194c192ae
DESKTOP-0PA8STI\root <mr.alan.china@gmail.com>
2018-07-10 22:12:03 +0800

    添加GL Utility。

---
+++ BlackFireFramework.Unity/Assets/BlackFireFramework/Build-In/Runtime/Script/Common/Utility/Utility.GL.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2018 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFireFramework.Unity
+{
+    public static partial class Utility
+    {
+        public static class GL
+        {
+
+            //public static void MeshBuilder(Mesh mesh,List<Vector3> vertices,)
+            //{
+            //    mesh.SetVertices(vertices);
+            //    //mesh.SetTangents.
+            //}
+
+
+        }
+
+    }
+}
