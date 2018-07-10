1e10e3168ab6a599a73bfd4d2502873625252b52
DESKTOP-P80QGVB\Mr-Alan <mr.alan.china@gmail.com>
2018-07-11 01:50:49 +0800

    暂停GL想法。

--- BlackFireFramework.Unity/Assets/BlackFireFramework/Build-In/Runtime/Script/Common/Utility/Utility.GL.cs
+++ BlackFireFramework.Unity/Assets/BlackFireFramework/Build-In/Runtime/Script/Common/Utility/Utility.GL.cs
@@ -14,12 +14,6 @@ namespace BlackFireFramework.Unity
         public static class GL
         {
 
-            //public static void MeshBuilder(Mesh mesh,List<Vector3> vertices,)
-            //{
-            //    mesh.SetVertices(vertices);
-            //    //mesh.SetTangents.
-            //}
-
 
         }
 
--- BlackFireFramework.Unity/Assets/BlackFireFramework/Demo/GL/ModelBase.cs
+++ BlackFireFramework.Unity/Assets/BlackFireFramework/Demo/GL/ModelBase.cs
@@ -1,56 +1,46 @@
-﻿ //----------------------------------------------------
+﻿//----------------------------------------------------
 //Copyright © 2008-2018 Mr-Alan. All rights reserved.
 //Mail: Mr.Alan.China@[outlook|gmail].com
 //Website: www.0x69h.com
 //----------------------------------------------------
 
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 
 namespace BlackFireFramework.Unity
 {
     [RequireComponent(typeof(MeshFilter))]
     [RequireComponent(typeof(MeshRenderer))]
-	public sealed class GLDemo : MonoBehaviour 
+    public abstract class ModelBase : MonoBehaviour 
 	{
+
         private MeshFilter m_MeshFilter = null;
         private MeshRenderer m_MeshRenderer = null;
         private Mesh m_Mesh = null;
-        [SerializeField] private List<Vector3> m_Vertices;
-        [SerializeField] private int[] m_Triangles;
-        [SerializeField] private Vector2[] m_UV;
+        //private List<Face> m_FaceList = new List<Face>();
 
-
-        private List<Vector3> m_Origin = new List<Vector3>();
-        private List<Vector3> m_Update = new List<Vector3>();
-
-
-        private void Awake()
+        protected virtual void Awake()
         {
             m_Mesh = new Mesh();
             m_MeshFilter = GetComponent<MeshFilter>();
             m_MeshRenderer = GetComponent<MeshRenderer>();
             m_MeshFilter.mesh = m_Mesh;
-
-            for (int i = 0; i < 1000; i+=10)
-            {
-                for (int j = 0; j < 1000; j+=10)
-                {
-                    m_Origin.Add(new Vector3(i,j,0));
-                }
-            }
-
-
         }
 
-        private void Update()
+        protected virtual void Update()
         {
-            
+            OnPost();
+        }
+
+        protected virtual void OnPost()
+        {
+
         }
 
 
     }
+
+
+   
 }
