52dd358e1fb54d1b4090b1795ee671b1d853fb82
DESKTOP-8FL3M3V\Alan <835988221@qq.com>
2018-08-29 14:54:06 +0800

    添加LoopManager。

---
+++ BlackFireFramework.Unity/Assets/BlackFireFramework/Build-In/Runtime/Script/Manager/Loop/BlackFire.Lazy.cs
@@ -0,0 +1,14 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2018 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+using BlackFireFramework.Unity;
+
+public sealed partial class BlackFire
+{
+    private static ILoopManager s_Loop = null;
+    public static ILoopManager Loop { get { return s_Loop = (s_Loop ?? GetManager<ILoopManager>()); } }
+
+}
---
+++ BlackFireFramework.Unity/Assets/BlackFireFramework/Build-In/Runtime/Script/Manager/Loop/Extension/ILoopManagerExtension.cs
@@ -0,0 +1,38 @@
+﻿//----------------------------------------------------
+//Copyright © 2008-2017 Mr-Alan. All rights reserved.
+//Mail: Mr.Alan.China@[outlook|gmail].com
+//Website: www.0x69h.com
+//----------------------------------------------------
+
+using System;
+
+namespace BlackFireFramework.Unity
+{
+    /// <summary>
+    /// LoopComponent扩展类。
+    /// </summary>
+    public static class ILoopManagerExtension
+	{
+        public static void Register(this ILoopManager loop, Action callback, LoopType loopType, string name = null)
+        {
+            loop.Register(callback as Delegate, loopType,name);
+        }
+
+        #region 测试
+        //Not often used ... 
+
+        public static void Register(this ILoopManager loop, Action<int> callback, LoopType loopType, string name = null)
+        {
+            loop.Register(callback as Delegate, loopType, name);
+        }
+
+        public static void Register(this ILoopManager loop, Action<int, int> callback, LoopType loopType, string name = null)
+        {
+            loop.Register(callback as Delegate, loopType, name);
+        }
+
+        //Not often used ...
+        #endregion
+
+    }
+}
